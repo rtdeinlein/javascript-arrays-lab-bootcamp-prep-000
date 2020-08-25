919a5caf5a4c8390d819dd3faa0700c1a4feb863
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-25 18:27:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(Milo){
   return kittens.push(Milo)
 }
+function destructivelyPrependKitten(Milo){
+  return kittens.unshift(Milo)
+}
